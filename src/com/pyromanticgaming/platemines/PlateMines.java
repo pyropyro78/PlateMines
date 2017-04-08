9c80fe7a9fc13ab00e34eff07d122c44fb145b6a
Bradley Van Dyne <pyropyro78@gmail.com>
2017-04-07 23:27:03 -0700

    Fixed copyright and added a check for defuse tool

--- src/com/pyromanticgaming/platemines/PlateMines.java
+++ src/com/pyromanticgaming/platemines/PlateMines.java
@@ -1,20 +1,7 @@
 package com.pyromanticgaming.platemines;
 
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-
 /*
- *Copyright (c) <2016>, <pyropyro78>, <pyropyro78@gmail.com>
- *All rights reserved.
- *
- *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-import org.bukkit.entity.Player;
-
-/*
- *Copyright (c) <2016>, <pyropyro78>, <pyropyro78@gmail.com>
+ *Copyright (c) <2016-2017>, <pyropyro78>, <pyropyro78@gmail.com>
  *All rights reserved.
  *
  *
@@ -28,6 +15,10 @@ import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 
 public final class PlateMines extends JavaPlugin implements Listener{
 
@@ -186,7 +177,7 @@ public final class PlateMines extends JavaPlugin implements Listener{
 		 * Finish config to let server owners know that this is a global check regardless of player mines
 		 * 
 		 */
-		if (event.getClickedBlock().getType().name().equals("STONE_PLATE") && p.getItemInHand().getType().equals(Material.SHEARS)) {
+		if (event.getClickedBlock().getType().name().equals("STONE_PLATE") && p.getItemInHand().getType().equals(Material.SHEARS) && ForceDefuseTool) {
 			Block block = event.getClickedBlock().getLocation().getBlock();
 			Location blockloc = event.getClickedBlock().getLocation();
 			Material blockmat = block.getState().getType();
